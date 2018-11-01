ecf5656dd3dcb400d1c5d8b1498b35fbf9cac27e
dev <tomas.pospisil180695@gmail.com>
2018-11-01 21:29:13 +0100

    generovani kulicky

---
+++ kulička/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace kulička
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnKulicka = new System.Windows.Forms.Button();
+            this.casovac = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnKulicka
+            // 
+            this.btnKulicka.Location = new System.Drawing.Point(166, 184);
+            this.btnKulicka.Name = "btnKulicka";
+            this.btnKulicka.Size = new System.Drawing.Size(75, 49);
+            this.btnKulicka.TabIndex = 0;
+            this.btnKulicka.Text = "Nová kulička";
+            this.btnKulicka.UseVisualStyleBackColor = true;
+            this.btnKulicka.Click += new System.EventHandler(this.btnKulicka_Click);
+            // 
+            // casovac
+            // 
+            this.casovac.Enabled = true;
+            this.casovac.Interval = 50;
+            this.casovac.Tick += new System.EventHandler(this.casovac_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(60, 184);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(60, 212);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 191);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "vx:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 219);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "vy:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnKulicka);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKulicka;
+        private System.Windows.Forms.Timer casovac;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ kulička/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace kulička
+{
+    public partial class Form1 : Form
+    {
+        Random náhoda = new Random();
+
+        //souřadnice středu kuličky
+        // (na začátku "za okrajem", aby nebyla vidět)
+        double x = -20, y = -20;
+        //Složky rychlosti kuličky (na začátku stojí)
+        double vx = 0, vy = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics kp = e.Graphics;
+            //FillElipse potřebuje levý horní (LH) roh
+            //opsaného obdelníka. Poloměr kuličky bude 5 pixelů
+
+            int xLH = Convert.ToInt32(x - 5);
+            int yLH = Convert.ToInt32(y - 5);
+
+            kp.FillEllipse(Brushes.CornflowerBlue, xLH, yLH, 10, 10);
+        }
+
+        private void casovac_Tick(object sender, EventArgs e)
+        {
+            double čas = casovac.Interval * 0.001;
+            x += vx * čas;
+            y += vy * čas;
+            Refresh();
+        }
+
+        private void btnKulicka_Click(object sender, EventArgs e)
+        {
+
+            //Kulička se umístí (vrátí) doprostřed okna
+            x = ClientSize.Width / 2;
+            y = ClientSize.Height / 2;
+
+            //náhodně určíme rychlost kuličky
+            vx = Convert.ToInt32(textBox1.Text);
+            vy = Convert.ToInt32(textBox2.Text);
+
+
+
+
+        }
+    }
+}
